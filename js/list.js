814155fead429b283f7d69a07c0e304247e12e95
aa4090201 <91879943+aa4090201@users.noreply.github.com>
2022-06-19 18:15:52 +0800

    Update list.js

--- js/list.js
+++ js/list.js
@@ -1,4 +1,4 @@
-const URL = 'https://script.google.com/macros/s/AKfycbyjR1hxReqBWPN8H7wqrxg14kEfGjdXBGFXgQJ7uoGx6OSZy3o_1lR8cp223UO2p1Ly/exec';
+const URL = 'https://script.google.com/macros/s/AKfycbwlh0WHnw0NuLJl0p8iI05ignH71FszuFljZ14l7A8Fot3PjIm_urZX1e28S2i9g00i/exec';
 
 $(document).ready(function() {
 	loadData();
@@ -13,6 +13,17 @@ function loadData(){
 			let userData = data.data;
 			console.log(userData)
 			for(let i=0;i<userData.length;i++){
+				let url2 = JSON.stringify(userData[i].enddate);
+				let ary = url2.split('T');
+				let sid = ary[0];
+				sid = sid.split('"');
+				userData[i].enddate = sid[1];
+				if(userData[i].enddate == undefined){
+					userData[i].enddate = "無";
+				}
+				if(userData[i].date == undefined){
+					userData[i].date = "無";
+				}
 				let content = oneRow(i+1, userData[i]);
 				$('tbody').append(content);
 			}
@@ -28,16 +39,20 @@ function loadData(){
 function oneRow(n, man){
 	let html = `
 				<tr>
-					<th scope="row">${n}</th>
+					<th class="ms-5" scope="row">${n}</th>
 					<td>${man.gameName}</td>
 					<td>${man.discount}%off</td>
 					<td>${man.platformType}</td>
-					<td>${man.enddate}</td>
+				
+					<td class="th-time">${man.enddate}</td>
 					<td>NT$${man.nowprice}</td>
 					<td>NT$${man.originalprice}</td>
 					<td>NT$${man.lowestprice}</td>
 					<td>${man.evaluation}%好評</td>
-					<td>${man.experience}</td>
+					<td class="me-5">${man.experience}</td>
 				</tr>`;
+					// <td class="th-time">${man.date}</td>
 	return html;
 }
+
+
